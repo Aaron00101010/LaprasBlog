447f59c737430dad740864e0bb19d5ee16ffdaac
xieqiang <xieqiang9408@gmail.com>
2018-05-15 18:04:51 +0800

    init: client page

---
+++ client/src/components/page/PArticalList.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class='artical-list-box'>
+        artical-list1
+    </div>
+</template>
+<script>
+    export default {
+      name: 'PArticalList'
+    }
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ client/src/components/page/PTags.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class='tags'>
+        tags
+    </div>
+</template>
+<script>
+    export default {
+      name: 'PTags'
+    }
+</script>
+<style lang="scss" scope>
+</style>
