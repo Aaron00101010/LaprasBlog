1b9257dcf4a76de532287f375180bfdd95c90a40
xieqiang <xieqiang9408@gmail.com>
2018-05-18 13:24:30 +0800

    add:admin page init

---
+++ admin/src/components/page/PEditor.vue
@@ -0,0 +1,53 @@
+<template>
+    <el-container id="editor">
+      <el-aside class="artical-list-wrapper" width='200px'>
+        <m-editor-artical-list/>
+      </el-aside>
+      <el-container>
+        <el-header class="editor-setting-wrapper" style="height:auto">
+            <m-editor-setting />
+        </el-header>
+        <el-main class="editor-wrapper">
+           <mavon-editor />
+        </el-main>
+      </el-container>
+    </el-container>
+</template>
+<script>
+import { mavonEditor } from 'mavon-editor'
+import MEditorArticalList from '@/components/mod/MEditorArticalList'
+import MEditorSetting from '@/components/mod/MEditorSetting'
+import 'mavon-editor/dist/css/index.css'
+export default {
+  name: 'PEditor',
+  components: {
+    mavonEditor,
+    MEditorArticalList,
+    MEditorSetting
+  }
+}
+</script>
+<style lang='scss' scoped>
+#editor {
+  margin: auto;
+  width: 100%;
+  height: 100%;
+}
+.artical-list-wrapper{
+  border-right: 1px solid #eee;
+
+}
+.editor-setting-wrapper{
+  padding:0;
+  border-bottom: 1px solid #eee;
+  height: auto;
+}
+.editor-wrapper{
+  padding: 0;
+  display: flex;
+  align-items: stretch;
+  &>*{
+    width: 100%;
+  }
+}
+</style>
--- admin/src/components/page/PLogin.vue
+++ admin/src/components/page/PLogin.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="login-wrapper">
       <el-card class="box-card">
-        <h2>用户登录</h2>
+        <h2 class='login-tit'>用户登录</h2>
         <form action="./2" >
           <el-input v-model="username" placeholder="请输入用户名"></el-input>
           <el-input type="password" v-model="password" placeholder="请输入密码"></el-input>
           <el-col :span='20' :offset='2'>
-            <el-button native-type='button' type="primary">登录</el-button>
+            <router-link :to="{name:'home'}">
+             <el-button native-type='button' type="primary">登录</el-button>
+            </router-link>
           </el-col>
         </form>
       </el-card>
@@ -24,13 +26,19 @@ export default{
   }
 }
 </script>
-<style>
+<style lang='scss' scoped>
 .box-card{
   width: 400px;
+  margin-top: 120px;
+  text-align: center;
 }
 .login-wrapper{
   display: flex;
   justify-content: center;
+}
+.login-tit{
+  margin-bottom:20px;
+  line-height: 1;
 }
 .el-row{
   justify-content: center;
