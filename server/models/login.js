50ede8974c9b3a6f1a5d71fcd5441bbeb1efb0ed
Aaron <xieqiang9408@gmail.com>
2018-08-12 18:57:21 +0800

    fix: sql error by table name case

--- server/models/login.js
+++ server/models/login.js
@@ -3,7 +3,7 @@ const queryDatabase = require('../utils/queryDatabase')
 class Login {
   async getPassword (userName) {
     const res = await queryDatabase(
-      `SELECT password FROM USER WHERE userName='${escape(
+      `SELECT password FROM user WHERE userName='${escape(
         String(userName).trim()
       )}'`
     )
